da886ce6cbe2abcb8ed099bf7b3fe2270235e5af
Layton Whiteley <layton.whiteley@gmail.com>
2014-04-10 22:10:18 -0400

    update architecture

--- app/scripts/features/game/controllers/_core.js
+++ app/scripts/features/game/controllers/_core.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('fiyuh.game').controller('GamesCtrl', ['$scope', 'Restangular', '$log', function ($scope, Restangular, $log) {
+angular.module('app.game').controller('GamesCtrl', ['$scope', 'Restangular', '$log', function ($scope, Restangular, $log) {
     Restangular.all('awesomeThings').getList().then(function(result){
         $scope.awesomeThings = result;
         $log.info(result);
